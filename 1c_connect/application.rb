429a8b6e06860aeca6dca5cbcd70bfcc6ddcd6eb
Antonichev Maksym <novomax@ukr.net>
2025-03-11 22:44:52 +0100

    Delete 1c_connect directory

--- 1c_connect/application.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'config/configuration'
-
-class Application < Sinatra::Base
-  use Controllers::Main
-end
--- 1c_connect/config/configuration.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra/base'
-require 'slim'
-require 'erubis'
-require 'win32ole'
-
-require_relative '../controllers/main'
